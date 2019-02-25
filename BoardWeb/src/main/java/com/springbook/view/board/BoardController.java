b54209f958657708ce7714aea2371a4672c07ed7
LENOVO <LENOVO@LENOVO-PC>
2019-02-26 08:52:19 +0900

    다국어 처리 추가

---
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -0,0 +1,83 @@
+package com.springbook.view.board;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+
+@Controller
+@SessionAttributes("board")
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+
+	@RequestMapping(value="/insertBoard.do")
+	public String insertBoard(BoardVO vo) throws IOException{
+		
+		// 파일 업로드 처리
+		MultipartFile uploadFile = vo.getUploadFile();
+		if(!uploadFile.isEmpty()) {
+			String fileName = uploadFile.getOriginalFilename();
+			uploadFile.transferTo(new File("C:/" + fileName));
+		}
+		
+		boardService.insertBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
+		boardService.updateBoard(vo);
+		return "getBoardList.do";
+	}	
+	
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo)); 	 // Model 정보 저장
+		return "getBoard.jsp";
+	}
+	
+	@RequestMapping("/getBoardList.do")
+	public String getBoardList(BoardVO vo, Model model) {
+
+		if(vo.getSearchCondition() == null) {
+			vo.setSearchCondition("TITLE");
+		}
+		if(vo.getSearchKeyword() == null) {
+			vo.setSearchKeyword("");
+		}else {
+			System.out.println(vo.getSearchCondition());
+			System.out.println(vo.getSearchKeyword());
+		}
+		model.addAttribute("boardList", boardService.getBoardList(vo)); 	// Model 정보 저장
+		return "getBoardList.jsp";
+	}
+
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap(){
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+
+		return conditionMap;
+	}
+	
+}	
+
